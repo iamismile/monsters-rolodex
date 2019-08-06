ee522163d8f7324a5040f13a50259c441826b35e
Ismile Hossain <ismile52hssn@gmail.com>
2019-08-06 06:17:05 +0600

    Updates

---
+++ precache-manifest.b9ffc6e81219c8724f90f9d69084ec38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbf46ec80c4b8bfe54d6c4e7e34bc40",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45dcf1efafe60f7a7180",
+    "url": "/monsters-rolodex/static/css/main.e3381e8f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "45dcf1efafe60f7a7180",
+    "url": "/monsters-rolodex/static/js/main.9f704ada.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
